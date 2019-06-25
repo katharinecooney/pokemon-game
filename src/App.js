738c16eec94134300968308fca6264b5ebcde4be
Katharine Cooney <katharinecooney@gmail.com>
2019-06-25 08:15:16 -0500

    Pass props from App to Pokecard; add logic to Pokecard component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Pokedex from './Pokedex';
+import Pokecard from './Pokecard';
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Pokedex />
+				<Pokecard id={4} name='Charmander' type= 'fire' base_experience={62} />
 			</div>
 		);
 	}
